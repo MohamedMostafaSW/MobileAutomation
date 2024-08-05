9b7fab2fce5af733c170070aa0ad4cc2557ed554
Mohamed Mostafa <mohamed.yassin7159@gmail.com>
2024-08-05 21:26:23 +0300

    another success day

--- src/main/java/locatorsAndFunctionsGeneralStore/LoginPage.java
+++ src/main/java/locatorsAndFunctionsGeneralStore/LoginPage.java
@@ -1,6 +1,5 @@
 package locatorsAndFunctionsGeneralStore;
 
-import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.pagefactory.AndroidFindBy;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
